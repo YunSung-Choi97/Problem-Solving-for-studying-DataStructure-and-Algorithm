92183929159979ee2dc1e6a233426cf253a1a8f5
YunSung-Choi97 <sunsu6765@gmail.com>
2021-02-24 18:34:33 +0900

    BOJ 10773

--- 17 스택/02 제로.py
+++ 17 스택/02 제로.py
@@ -1,8 +1,8 @@
-Num_lst = []  # 입력받은 정수를 저장할 리스트. 0을 입력받을 경우 가장 최근의 수를 지움
-N = int(input())  # 입력받을 정수의 개수를 입력받음
+N = int(input())  # N : 입력받을 정수의 개수
+Num_lst = []  # Num_lst : 입력받은 정수를 저장할 리스트. 0을 입력받을 경우 가장 최근의 수를 지움
 
-for i in range(N):  # N번 수행
-    num = int(input())  # 입력받은 정수를 저장
+for i in range(N):  # N번 반복
+    num = int(input())  # num : 입력받은 정수를 할당
 
     # 입력받은 수가 0일 경우
     if not num:
