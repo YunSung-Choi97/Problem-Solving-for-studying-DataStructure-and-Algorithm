c084068240b9ad073d9b8d31d1fde1b70b7986f4
YunSung-Choi97 <sunsu6765@gmail.com>
2021-02-28 19:59:20 +0900

    BOJ 11022

---
+++ 03 for문/08 A+B (8).py
@@ -0,0 +1,5 @@
+T = int(input())  # T : 테스트 케이스 개수
+
+for i in range(1, T+1):  # T번 반복. i : 1 ~ T
+    n, m = map(int, input().split())
+    print("Case #{}: {} + {} = {}".format(i, n, m, n+m))  # " ".format() : 문자열에 .format을 통해 {}부분에 원하는 내용을 삽입가능
