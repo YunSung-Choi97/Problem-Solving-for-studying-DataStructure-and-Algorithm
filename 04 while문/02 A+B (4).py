098967f55cf951df1a6ae1ad9b90f61be4e82121
YunSung-Choi97 <sunsu6765@gmail.com>
2021-03-01 21:53:18 +0900

    BOJ 10951

--- 04 while문/02 A+B (4).py
+++ 04 while문/02 A+B (4).py
@@ -1,8 +1,9 @@
 while True:  # 무한 반복
     
+    # 무한 루프의 종료조건이 문제에 제시되었지 않았기 때문에, 에러가 발생하기 전까지는 프로그램이 동작하도록 해주었다.
     # try, except : 에러가 발생할 경우에 대한 처리
     try:
-        num1, num2 = map(int, input().split())
-        print(num1+num2)
+        A, B = map(int, input().split())
+        print(A+B)
     except:
         break
