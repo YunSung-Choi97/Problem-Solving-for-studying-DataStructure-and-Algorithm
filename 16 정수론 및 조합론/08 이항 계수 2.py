dc4c7d7ae17dabd1e8b432a7c42b51d329286198
YunSung-Choi97 <sunsu6765@gmail.com>
2021-04-04 11:11:29 +0900

    BOJ 11051

---
+++ 16 정수론 및 조합론/08 이항 계수 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+# (N)! / ((N-K)! * K!)
+answer = 1
+for i in range(K):
+    answer = answer * (N-i) // (i+1)
+
+print(answer % 10007)
