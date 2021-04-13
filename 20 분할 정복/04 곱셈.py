d82050495f2831c4e420713858ad1f37072c013d
YunSung-Choi97 <sunsu6765@gmail.com>
2021-04-13 13:10:03 +0900

    BOJ 1629

--- 20 분할 정복/04 곱셈.py
+++ 20 분할 정복/04 곱셈.py
@@ -1,6 +1,9 @@
 import sys
 input = sys.stdin.readline
 
+# A^N 계산할 때
+# N이 짝수라면 (A^(N//2))^2을 이용해 계산을 하고
+# N이 홀수라면 (A^(N//2))^2*A를 이용해 계산을 해주었다.
 def multiplication_R_by_power(a, b, c):
     if b == 1:
         return a % c
