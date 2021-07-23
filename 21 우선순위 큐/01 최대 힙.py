5350947390e3bde8fbd3c60e2fad4940f5e9b2fb
YunSung-Choi97 <sunsu6765@gmail.com>
2021-07-23 23:56:04 +0900

    BOJ 11279

---
+++ 21 우선순위 큐/01 최대 힙.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())  # N : 입력받을 연산의 개수
+heap = []  # 
+
+for i in range(N):
+    asdf
+
+
+
+
+
+# 나중에 정리할 때 확인
+# 파이썬에서 표준 라이브러리로 제공하는
+# PriorityQueue / heapq
+# heapq가 통상적으로 조금 더 빠르게 동작
