f882e2ac4588e52a172055bbff4461baaaf308a0
YunSung-Choi97 <sunsu6765@gmail.com>
2021-07-30 14:07:18 +0900

    Chap3. 3-2

---
+++ Chap03_Greedy/Ans3-2-1.py
@@ -0,0 +1,27 @@
+# 단순하게 푸는 답안 예시
+# M이 크기가 커질 경우 시간 초과가 발생할 수 있다.
+# 따라서 반복되는 수열을 파악하여 2번풀이를 이용하여 푸는 것이 안정적이다.
+
+# N, M, K를 공백을 기준으로 구분하여 입력 받기
+n, m, k = map(int, input().split())
+# N개의 수를 공백을 기준으로 구분하여 입력 받기
+data = list(map(int, input().split()))
+
+data.sort() # 입력받은 수들 정렬하기
+first = data[n-1] # 가장 큰 수
+second = data[n-2] # 두 번째로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k): # 가장 큰 수를 K번 더하기
+        if m == 0: # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1 # 더할 때마다 1씩 빼기
+    if m == 0: # m이 0이라면 반복문 탈출
+        break
+    result += second # 두 번째로 큰 수를 한 번 더하기
+    m -= 1 # 더할 때마다 1씩 빼기
+
+print(result) # 최종 답안 출력
