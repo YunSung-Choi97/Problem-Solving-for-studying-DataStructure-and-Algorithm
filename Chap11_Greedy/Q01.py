e5f9aa03678a8e79a79af280714e0d946900da9b
YunSung-Choi97 <sunsu6765@gmail.com>
2021-08-03 17:03:03 +0900

    Chap11. Q01

--- Chap11_Greedy/Q01.py
+++ Chap11_Greedy/Q01.py
@@ -8,14 +8,10 @@ group = []
 for i in data:
     if len(group) == 0:
         group.append(i)
-        continue
     elif group[-1] > len(group):
         group.append(i)
-        continue
     else:
         groups.append(group)
         group = []
-        continue
 
-# print(groups)
 print(len(groups))    
