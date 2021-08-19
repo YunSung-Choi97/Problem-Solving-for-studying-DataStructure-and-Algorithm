041c51daafb89ca7f841279925379082beb15dd4
YunSung-Choi97 <sunsu6765@gmail.com>
2021-08-19 18:40:46 +0900

    BOJ 11651

---
+++ 12 정렬/07 좌표 정렬하기 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+xy_data = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    xy_data.append((x, y))
+
+# xy_data에서 각각의 0번 인덱스 값으로 오름차순 정렬을 한 뒤 1번 인덱스 값으로 오름차순 재정렬
+xy_data.sort()
+xy_data.sort(key=lambda num : num[1])
+for i in range(N):
+    print(xy_data[i][0], xy_data[i][1])
