6f81fd8c815e7a260e4673923fcbe5908758506c
YunSung-Choi97 <sunsu6765@gmail.com>
2021-08-26 22:59:24 +0900

    BOJ 10814

--- 12 정렬/09 나이순 정렬.py
+++ 12 정렬/09 나이순 정렬.py
@@ -1,15 +1,12 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# people = []
-# N = int(input())
-# for _ in range(N):
-#     age, name = input().split()
-#     people.append((age, name))
+people = []
+N = int(input())
+for _ in range(N):
+    age, name = input().split()
+    people.append((age, name))
 
-# people.sort(key=lambda human : int(human[0]))  # 나이를 기준으로 정렬
-# for human in people:
-#     print(human[0], human[1])
-
-a = list()
-print(a)
+people.sort(key=lambda human : int(human[0]))  # 나이를 기준으로 정렬
+for human in people:
+    print(human[0], human[1])
