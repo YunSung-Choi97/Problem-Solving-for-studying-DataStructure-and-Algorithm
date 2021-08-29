c5bdcf0996af6165fd885dbaa7ed9ddb416cdd6b
YunSung-Choi97 <sunsu6765@gmail.com>
2021-08-29 10:17:45 +0900

    BOJ 2208

--- 코테 스터디/8주차 - 구현, 누적합, 이분탐색, 스택or큐, dp/05 화장실의 규칙_4.py
+++ 코테 스터디/8주차 - 구현, 누적합, 이분탐색, 스택or큐, dp/05 화장실의 규칙_4.py
@@ -1,67 +1,95 @@
+# 결과 : 시간 초과
+
 import sys
 input = sys.stdin.readline
 
-def priority(array):
-    heap = []
-    heap.append(array[0])
-    for i in range(1, len(array)):
-        item = array[i]
-        idx = i
-        heap.append(item)
-        while True:
-            if idx == 0:
-                heap[0] = item
-                break
-            elif idx % 2 == 0:
-                if item > heap[idx//2-1]:
-                    heap[idx] = heap[idx//2-1]
-                    idx = idx//2-1
-                else:
-                    heap[idx] = item
-                    break
+# 입력받기 및 저장
+N, M, K = map(int, input().split())
+data = []
+for _ in range(N):
+    D, H = map(int, input().split())
+    data.append((D, H))
+
+
+new_data = [[] for _ in range(M)]  # new_data : M줄로 나눠선 줄 현황
+for i in range(len(data)):
+    new_data[i%M].append(data[i])
+
+first_D = dict()  # 선두의 근무 일수
+first_H = dict()  # 선두의 급한 정도
+for i in range(M):
+    if len(new_data[i]) != 0:
+        first_D[i] = new_data[i][0][0]
+        first_H[i] = new_data[i][0][1]
+
+location = [K // M, K % M]  # 데카의 위치
+
+search_idx_Dmax, search_idx_Hmax = True, True
+count = 0
+for _ in range(N):
+    # 먼저 사용할 사원 위치 찾기
+    if search_idx_Dmax:
+        idx_Dmax = [key for key, value in first_D.items() if value == max(first_D.values())]
+    if len(idx_Dmax) == 1:
+        idx = idx_Dmax[0]
+        case = 1
+    
+    else:
+        if search_idx_Hmax:
+            first_H_Dmax = {key : first_H[key] for key in idx_Dmax}
+            idx_Hmax = [key for key, value in first_H_Dmax.items() if value == max(first_H_Dmax.values())]
+        idx = idx_Hmax[0]
+        if len(idx_Hmax) == 1:
+            case = 2
+        else:
+            case = 3
+    
+    # 화장실 이용
+    if idx == location[1]:  # 데카의 줄에 있는 사람이 화장실을 이용하는 경우
+        count += 1
+        if location[0] - count < 0:
+            answer = _
+            break
+    
+    del new_data[idx][0]
+
+    # 시간을 줄여보기 위한 발악... (처음에는 이런 부분은 없었다..ㅠㅠ)
+    if len(new_data[idx]) == 0:
+        if case == 1:
+            search_idx_Dmax, search_idx_Hmax = True, True
+        elif case == 2:
+            idx_Dmax.remove(idx)
+            search_idx_Dmax, search_idx_Hmax = False, True
+        elif case == 3:
+            idx_Dmax.remove(idx)
+            idx_Hmax.remove(idx)
+            search_idx_Dmax, search_idx_Hmax = False, False
+        del first_D[idx]
+        del first_H[idx]
+    else:
+        if case == 1:
+            if first_D[idx] <= new_data[idx][0][0]:
+                search_idx_Dmax, search_idx_Hmax = False, False
             else:
-                if item > heap[idx//2]:
-                    heap[idx] = heap[idx//2]
-                    idx = idx//2
+                search_idx_Dmax, search_idx_Hmax = True, True
+        elif case == 2 or case == 3:
+            if first_D[idx] < new_data[idx][0][0]:
+                idx_Dmax = [idx]
+                search_idx_Dmax, search_idx_Hmax = False, False
+            elif first_D[idx] == new_data[idx][0][0]:
+                if first_H[idx] < new_data[idx][0][1]:
+                    idx_Hmax = [idx]
+                    search_idx_Dmax, search_idx_Hmax = False, False
+                elif first_H[idx] == new_data[idx][0][1]:
+                    search_idx_Dmax, search_idx_Hmax = False, False
                 else:
-                    heap[idx] = item
-                    break
-    result = []
-    for i in range(len(heap)-1):
-        result.append(heap[0])
-        item = heap[-1]
-        heap[0] = item
-        del heap[-1]
-        idx = 0
-        last = len(heap)-1
-        while True:
-            if idx*2+1 > last:  # 자식노드 없는 경우
-                heap[idx] = item
-                break
-            elif (idx+1)*2 > last:  # 자식노드가 left만 존재
-                if item < heap[idx*2+1]:
-                    heap[idx] = heap[idx*2+1]
-                    idx = idx*2+1
-                else:
-                    heap[idx] = item
-                    break
-            elif heap[idx*2+1] > heap[(idx+1)*2]:
-                if item < heap[idx*2+1]:
-                    heap[idx] = heap[idx*2+1]
-                    idx = idx*2+1
-                else:
-                    heap[idx] = item
-                    break
-            elif heap[idx*2+1] < heap[(idx+1)*2]:
-                if item < heap[(idx+1)*2]:
-                    heap[idx] = heap[(idx+1)*2]
-                    idx = (idx+1)*2
-                else:
-                    heap[idx] = item
-                    break
-    result.append(heap[0])
-    return result
+                    if case == 2:
+                        search_idx_Dmax, search_idx_Hmax = False, True
+                    else:
+                        idx_Hmax.remove(idx)
+            else:
+                search_idx_Dmax, search_idx_Hmax = True, True
+        first_D[idx] = new_data[idx][0][0]
+        first_H[idx] = new_data[idx][0][1]
 
-a = [1, 2, 3, 4, 6, 8, 5]
-
-print(priority(a))
+print(answer)
