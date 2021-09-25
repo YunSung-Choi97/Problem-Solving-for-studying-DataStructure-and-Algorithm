54e5373017a4f27254d5f80d60d2f5b728ab4b30
YunSung-Choi97 <sunsu6765@gmail.com>
2021-09-25 22:35:47 +0900

    BOJ 15652

--- 14 백트래킹/04_15652.py
+++ 14 백트래킹/04_15652.py
@@ -1,6 +1,11 @@
+'''미리 생각하기 [Try 2]
+1. 입력받기 (N, M)
+2. 정답(출력할 줄)을 한 자리씩 선정하기 (BackTracking)
+'''
+# N, M 입력받기
 N, M = map(int, input().split())
 
-answer = []
+answer = []  # 출력할 정답을 저장할 공간
 def BT(k):
     # M개의 숫자를 선택
     if len(answer) == M:
@@ -15,6 +20,10 @@ def BT(k):
 
 BT(1)
 
+'''미리 생각하기 [Try 1]
+1. 주어진 정보 입력받기 (N, M)
+2. itertools의 combinations_with_replacement를 이용
+'''
 ''' itertools의 combinations를 호출해 중복있는 조합을 만들어 출력
 from itertools import combinations_with_replacement
 
