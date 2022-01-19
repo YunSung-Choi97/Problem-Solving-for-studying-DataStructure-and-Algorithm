f0048dd52c1ec7ea0df36750ceb7c89ce2390717
YunSung-Choi97 <sunsu6765@gmail.com>
2022-01-19 23:58:52 +0900

    백준 2228번(구간 나누기)

---
+++ 백준-그룹스터디/21-08-29/2228.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+'''
+ex1)
+N 1 : NC1 + NC2 + NC3 + NC4 + NC5 + ... + NCN 중 1
+
+4 2 / 5 2 / 6 2 / 7 2 :
+...
+
+
+
+
+
+
+
+'''
