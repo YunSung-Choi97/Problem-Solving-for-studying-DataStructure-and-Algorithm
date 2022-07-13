3b58dc68dc020cfc341349fe636477b615c743b4
YunSung-Choi97 <sunsu6765@gmail.com>
2022-07-13 23:59:06 +0900

    20단계 큐, 덱

--- [백준] 단계별로풀어보기/20 큐, 덱/1966.py
+++ [백준] 단계별로풀어보기/20 큐, 덱/1966.py
@@ -1,27 +1,37 @@
-test_cases = int(input())
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-for _ in range(test_cases):
-    n,m = list(map(int, input().split( )))
-    imp = list(map(int, input().split( )))
-    idx = list(range(len(imp)))
-    idx[m] = 'target'
+T = int(input())  # 테스트케이스 개수
 
-    # 순서
-    order = 0
-    
-    while True:
-        # 첫번째 if: imp의 첫번째 값 = 최댓값?
-        if imp[0]==max(imp):
-            order += 1
-                        
-            # 두번째 if: idx의 첫 번째 값 = "target"?
-            if idx[0]=='target':
-                print(order)
+# 프로그램 실행
+for _ in range(T):
+    N, M = map(int, input().split())  # N(문서의 개수), M(궁금한 문서 위치)
+    importance = list(map(int, input().split()))  # 문서들의 중요도
+
+    printer = deque(importance.copy())  # 프린터에 현재 대기중인 문서들의 중요도
+    importance.sort()  # 프린터에 들어있는 중요도(오름차순)
+
+    count = 1  # 출력될 문서의 순서
+    rest = N  # 남은 문서의 수
+    while printer:
+        now = printer.popleft()
+        if M == 0:  # 현재 문서가 궁금한 문서인 경우
+            if now == importance[-1]:
+                result = count
                 break
             else:
-                imp.pop(0)
-                idx.pop(0)
-
+                printer.append(now)
+                M = rest - 1
+                continue
         else:
-            imp.append(imp.pop(0))
-            idx.append(idx.pop(0))
+            if now == importance[-1]:
+                importance.pop()
+                count += 1
+                rest -= 1
+            else:
+                printer.append(now)
+        M -= 1
+
+    # 결과 출력
+    print(result)
