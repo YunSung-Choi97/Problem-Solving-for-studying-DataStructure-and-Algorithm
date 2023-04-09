89a0c18aec22d703eb979d403ae83bab5c494367
YunSung-Choi97 <sunsu6765@gmail.com>
2023-04-09 23:59:14 +0900

    이코테 DFS/BFS 문제 1회차 (17, 18)

--- [이코테] 이것이 코딩 테스트다/Chap13 DFS와 BFS 문제/03 경쟁적 전염.py
+++ [이코테] 이것이 코딩 테스트다/Chap13 DFS와 BFS 문제/03 경쟁적 전염.py
@@ -1,4 +1,4 @@
-import copy
+from collections import deque
 
 N, K = map(int, input().split())  # 시험관 크기, 바이러스 종류
 board = []  # 시험관 정보
@@ -6,17 +6,17 @@ for _ in range(N):
     board.append(list(map(int, input().split())))
 S, Y, X = map(int, input().split())  # 증식 시간, 위치(X, Y)
 
-virus = [[] for _ in range(K + 1)]  # 현재 증식가능한 바이러스 종류별 위치: 번호가 낮은 바이러스부터 증식시키기 위해 저장
-new_virus = [[] for _ in range(K + 1)]  # 다음 증식가능한 바이러스 종류별 위치
+virus = []  # 바이러스 정보
 for y in range(N):
     for x in range(N):
         if board[y][x] != 0:
-            virus[board[y][x]].append((x, y))
-            new_virus[board[y][x]].append((x, y))
+            virus.append((board[y][x], (x, y), 0))
+virus.sort()  # 종류의 번호가 낮은 바이러스부터 증식하기위해 정렬
+virus = deque(virus)  # 정렬 후 큐에 저장
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-def increase_virus(x, y, virus_type):
+def increase_virus(x, y, virus_type, time):
     for d in range(4):
         nx, ny = x + dx[d], y + dy[d]
         # 시험관 내부가 아닌 경우 통과
@@ -26,14 +26,38 @@ def increase_virus(x, y, virus_type):
         if board[ny][nx] != 0:
             continue
         board[ny][nx] = virus_type  # 바이러스 증식
-        new_virus[virus_type].append((nx, ny))  # 추가된 바이러스 정보 저장
-    new_virus[virus_type].remove((x, y))  # 증식했기 때문에 이후에 증식이 불가능하므로 제거
+        virus.append((virus_type, (nx, ny), time + 1))  # 추가된 바이러스 정보 저장
 
-for time in range(S):  # S번 증식
-    # time초의 바이러스 증식 시작
-    for virus_type in range(1, K + 1):  # 바이러스 번호가 낮은 바이러스부터 차례로 증식
-        for x, y in virus[virus_type]:
-            increase_virus(x, y, virus_type)
-    virus = copy.deepcopy(new_virus)  # 증식된 바이러스 정보 불러오기
+while virus:
+    virus_type, location, time = virus.popleft()
+    # 확인 지점이 증식된 경우 탐색 종료
+    if location[0] == X - 1 and location[1] == Y - 1:
+        break
+    # 증식 시간이 지난 경우 탐색 종료
+    if time >= S:
+        break
+    increase_virus(location[0], location[1], virus_type, time)
 
 print(board[Y - 1][X - 1])  # 결과 출력
+
+''' 입력 예시 1
+3 3
+1 0 2
+0 0 0
+3 0 0
+2 3 2
+'''
+''' 출력 예시 1
+3
+'''
+
+''' 입력 예시 2
+3 3
+1 0 2
+0 0 0
+3 0 0
+1 2 2
+'''
+''' 출력 예시 2
+0
+'''
---
+++ [이코테] 이것이 코딩 테스트다/Chap13 DFS와 BFS 문제/04 괄호 변환.py
@@ -0,0 +1,55 @@
+# 두 "균형잡힌 괄호 문자열"을 반환
+def get_balanced_string(string):
+    open_bracket, close_bracket = 0, 0  # 여는 괄호 개수, 닫는 괄호 개수
+    for i in range(len(string)):
+        if string[i] == '(':
+            open_bracket += 1
+        else:
+            close_bracket += 1
+        # 두 균형잡힌 괄호 문자열로 구분하는 인덱스인 경우
+        if open_bracket == close_bracket:
+            u = string[:i + 1]
+            v = string[i + 1:]
+            break
+    return [u, v]
+
+# "올바른 괄호 문자열"인지 확인
+def is_collect_string(string):
+    stack = []
+    for char in string:
+        # 여는 괄호인 경우 stack에 저장
+        if char == '(':
+            stack.append(char)
+        # 닫는 괄호인 경우
+        else:
+            # 여는 괄호를 가지고 있는 경우 나머지 확인
+            if stack and stack.pop() == '(':
+                continue
+            # 여는 괄호를 가지고 있지 않은 경우 "올바른 괄호 문자열"이 아님
+            else:
+                return False
+    return True
+
+def solution(p):
+    # (규칙1) 빈 문자열인 경우, 빈 문자열을 반환
+    if p == '':
+        return ''
+    # (규칙2) 두 "균형잡힌 문자열"로 분리
+    u, v = get_balanced_string(p)
+    # (규칙3) u가 "올바른 괄호 문자열"인지 확인
+    if is_collect_string(u):
+        return u + solution(v)
+    # (규칙4)
+    else:
+        # (규칙4-1)
+        reverse_u = ''
+        for char in u:
+            if char == '(':
+                reverse_u += ')'
+            else:
+                reverse_u += '('
+        return '(' + solution(v) + ')' + reverse_u[1:-1]
+
+print(solution("(()())()"))
+print(solution(")("))
+print(solution("()))((()"))
